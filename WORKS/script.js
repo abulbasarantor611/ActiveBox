4dec90e8f999f0f7a4319ff7174a5cfc63809536
Abul-Basar-Antor <abulbasarantor23@gmail.com>
2023-11-06 20:26:07 +0600

    Project single page done...

---
+++ WORKS/script.js
@@ -0,0 +1,11 @@
+
+
+
+function controllMenu(){
+    document.getElementById('header-menu').classList.toggle('active');
+}
+
+
+
+var headerSection = document.getElementById('header-section');
+var headerContainer = document.getElementById('header-container');
